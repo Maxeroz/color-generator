fa69df4e5e5d6baaffd02b7bdd7acc18cfeb1309
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-02-20 21:53:23 +0400

    Color Generator Get Values

--- src/App.js
+++ src/App.js
@@ -10,7 +10,12 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello from submit");
+    setError(false);
+    try {
+      let colors = new Values(color).all(10);
+    } catch (error) {
+      setError(true);
+    }
   };
 
   return (
@@ -23,6 +28,7 @@ function App() {
             value={color}
             onChange={(e) => setColor(e.target.value)}
             placeholder="#f15025"
+            className={`${error ? "error" : null}`}
           />
           <button className="btn">submit</button>
         </form>
