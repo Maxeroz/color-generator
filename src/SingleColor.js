fb7f10871009acf19e119edc69e151fe9ce1fc1b
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-02-21 00:12:26 +0400

    Copy to Clipboard

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -6,13 +6,26 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const bcg = rgb.join(",");
   const hex = rgbToHex(...rgb);
   const hexValue = `#${hexColor}`;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+      return () => clearTimeout(timeout);
+    }, 3000);
+  }, [alert]);
+
   return (
     <article
       className={`color ${index > 10 && "color-light"}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </article>
   );
 };
